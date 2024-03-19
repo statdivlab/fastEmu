d711758d8c633fa2555aecf2c9779ec08ae5887b
Sarah Teichman <teichs@uw.edu>
2024-03-19 15:33:39 -0700

    rename `simulateNullBs` to `simulateBs` and add arguments for simulating under alternative

--- R/simulateBs.R
+++ R/simulateBs.R
@@ -1,7 +1,8 @@
 # function to simulate data under the null
 # adapted from `get_sim_bs` in `radEmu`
-simulateNullBs <- function(J, constraint_cats, test_j,
-                           constraint_mag, other_mag) {
+simulateBs <- function(J, constraint_cats, test_j,
+                       constraint_mag, other_mag,
+                       under_null = TRUE, alt_val = NULL) {
   # set b0 as in radEmu
   evens <- ((1:J)%%2 == 0)
   b0 <- numeric(J)
@@ -13,6 +14,12 @@ simulateNullBs <- function(J, constraint_cats, test_j,
     sinh(seq(-10, 10, length.out = length(constraint_cats)))/sinh(10)
   b1[-c(constraint_cats, test_j)] <- other_mag *
     sinh(seq(-10, 10, length.out = (J - 1 - length(constraint_cats))))/sinh(10)
-
+  if (!under_null) {
+    if (is.null(alt_val)) {
+      stop("If simulating under alternative, provide value for alternative B1")
+    } else {
+      b1[test_j] <- alt_val
+    }
+  }
   return(list(b0 = b0, b1 = b1))
 }
